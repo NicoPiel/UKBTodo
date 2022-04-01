fba60fce8c3e622784b961f86f893965e158c712
Nico Piel <nico.piel@hotmail.de>
2022-04-01 13:30:00 +0200

    Changed build config so standalone can use DB

--- src-electron/utility.js
+++ src-electron/utility.js
@@ -3,11 +3,12 @@ import { Sequelize } from 'sequelize';
 import { logger } from './logger/logger';
 import { app } from 'electron';
 
-export const DB_PATH = path.join(app.getAppPath(), 'db.db3');
+export const DB_PATH_DEBUG = path.join(app.getAppPath(), 'db.db3');
+export const DB_PATH = path.join(app.getPath('exe').slice(0, -12), 'db.db3');
 
 export let sequelize = new Sequelize({
     dialect: 'sqlite',
-    storage: DB_PATH,
+    storage: process.env.DEBUGGING ? DB_PATH_DEBUG : DB_PATH,
     pool: {
         max: 5,
         min: 0,
